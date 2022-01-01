603b0dc54214b1e372becc4cbf925d58f47a9d08
Reshul <53306036+resel143@users.noreply.github.com>
2022-01-01 15:22:58 +0530

    Update constructors.ts

--- constructors.ts
+++ constructors.ts
@@ -15,3 +15,24 @@ class Person{
 let p: Person = new Person("Reshul","Gomez",new Date(2000,3,30));
 
 p.show();
+
+
+// Graph system X, Y coordinates and its constructor initialization
+
+class Point{
+    x:number;
+    y:number;
+
+    constructor(x:number = 2, y:number=8){
+        this.x = x;
+        this.y = y;
+    }
+
+    draw= ()=>{console.log("X: "+ this.x + " Y: "+this.y)};
+
+
+
+}
+// a constructor is always called whenever the object is created!
+let point = new Point();
+point.draw();
